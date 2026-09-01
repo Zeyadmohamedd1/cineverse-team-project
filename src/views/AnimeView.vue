7d435d2330c85a17d2f31e1232604f3213703566
Hana Galal <hanagalal@students.du.edu.eg>
2026-09-01 19:52:40 +0300

    Add TV and Anime views

---
+++ src/views/AnimeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+    <h1>Anime</h1>
+    <p>عدد الانمي: {{ Anime.length }}</p>
+    <ul class="row row-cols-2 row-cols-md-3 row-cols-lg-5 list-unstyled">
+      <li class="col mb-4" v-for="item in Anime" :key="item.id">
+        <MediaCardComponent :item="item" />
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { getPopularAnime } from "@/services/tmdb";
+import MediaCardComponent from "@/components/MediaCardComponent.vue";
+
+export default {
+  components: {
+    MediaCardComponent,
+  },
+  data() {
+    return {
+      Anime: [],
+    };
+  },
+  async created() {
+    const data = await getPopularAnime();
+    this.Anime = data.results;
+  },
+};
+</script>
---
+++ src/views/TVShowsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+    <h1>TV Shows</h1>
+    <p>عدد المسلسلات: {{ shows.length }}</p>
+    <ul class="row row-cols-2 row-cols-md-3 row-cols-lg-5 list-unstyled">
+      <li class="col mb-4" v-for="show in shows" :key="show.id">
+        <MediaCardComponent :item="show" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getPopularTV } from '@/services/tmdb';
+import MediaCardComponent from '@/components/MediaCardComponent.vue';
+export default {
+    components:{
+        MediaCardComponent
+    },
+    data(){
+        return{
+            shows: []
+        }
+    },
+    async created(){
+        const data=await getPopularTV()
+        this.shows=data.results
+    }
+}
+</script>
