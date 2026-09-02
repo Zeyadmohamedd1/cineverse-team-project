a6338070238c6785b0aad82e99297afcab2693e6
Yousef Anis <yousefanis881@gmail.com>
2026-09-02 15:15:27 +0300

    add movies view

---
+++ src/views/MoviesView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mt-5">
+        <h1>Movies</h1>
+        <p>عدد الأفلام: {{ movies.length }}</p>
+        <ul class="row row-cols-2 row-cols-md-3 row-cols-lg-5 list-unstyled">
+            <li class="col mb-4" v-for="movie in movies" :key="movie.id">
+                <MediaCardComponent :item="movie" />
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import { getPopularMovies } from "@/services/tmdb";
+import MediaCardComponent from "@/components/MediaCardComponent.vue";
+
+export default {
+    components: {
+        MediaCardComponent,
+    },
+    data() {
+        return {
+            movies: [],
+        };
+    },
+    async created() {
+        const data = await getPopularMovies();
+        this.movies = data.results;
+    },
+};
+</script>
